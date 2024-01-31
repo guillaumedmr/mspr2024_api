d91d4ade2f0c2b1c2d99d50f881b6c92b80bb7c9
GuillaumeDMR <guillaume@daumur.fr>
2024-01-31 17:29:50 +0100

    signup auth

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, jsonify, request
-from sqlalchemy.exc import SQLAlchemyError
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 from config import Config
 from models import db, Utilisateurs
 
 app = Blueprint('auth_routes', __name__)
+
+bcrypt = Bcrypt()
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -17,18 +20,28 @@ def signup():
         email = data.get('email')
         mot_de_passe = data.get('mot_de_passe')
 
+        existing_user = Utilisateurs.query.filter_by(email=email).first()
+        if existing_user:
+            return jsonify(message='Cet utilisateur existe déjà! Veuillez choisir un autre email.'), 400
+
+        hashed_password = bcrypt.generate_password_hash(mot_de_passe).decode('utf-8')
+
         new_user = Utilisateurs(
             prenom=prenom, 
             nom=nom,
             dateNaissance=dateNaissance,
             email=email,
-            mot_de_passe=mot_de_passe
+            mot_de_passe=hashed_password
         )
 
         db.session.add(new_user)
         db.session.commit()
 
         return jsonify(message='Inscription réussie!')
+
+    except IntegrityError as e:
+        db.session.rollback()
+        return jsonify(message='Erreur d\'intégrité: Cet utilisateur existe déjà!'), 400
 
     except Exception as e:
         return jsonify(error=f"Une erreur s'est produite : {str(e)}"), 500
