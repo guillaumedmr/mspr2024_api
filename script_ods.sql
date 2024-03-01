e267af66ba3096c0f892404ad1f09f8aebda6cd2
GuillaumeDMR <guillaume@daumur.fr>
2024-03-01 15:51:26 +0100

    [ADD] Finish project

--- script_ods.sql
+++ script_ods.sql
@@ -3,5 +3,18 @@ CREATE TABLE ods_image (
     image_blob LONGBLOB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     coordinates VARCHAR(255) NULL,
-    user VARCHAR(255) NOT NULL
+    espece VARCHAR(255) NULL,
+    user VARCHAR(255) NULL
+);
+
+CREATE TABLE ods_infos_espece (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    espece VARCHAR(255),
+    description TEXT,
+    nom_latin VARCHAR(255),
+    famille VARCHAR(255),
+    region VARCHAR(255),
+    habitat TEXT,
+    fun_fact TEXT,
+    taille_cm FLOAT
 );
